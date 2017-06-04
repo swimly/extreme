b133c05209f18e4d83385c206b7f33c01d1ed19d
swimly <979741120@qq.com>
2017-06-04 12:56:42 +0800

    图片懒加载

---
+++ js/main.js
@@ -0,0 +1,18 @@
+;(function(){
+  var mySwiper = new Swiper ('.banner', {
+    direction: 'horizontal',
+    loop: true,
+    autoplay: 3000,
+    speed: 500,
+    // 如果需要分页器
+    pagination: '.swiper-pagination'
+  }) 
+  var mySwiper = new Swiper ('.advert', {
+    direction: 'horizontal',
+    loop: true,
+    autoplay: 4000,
+    speed: 300,
+    // 如果需要分页器
+    pagination: '.swiper-pagination'
+  })
+})()
